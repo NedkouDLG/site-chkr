0d1c4f93697ede8d1e9ae44b2b28c7d86af306a4
Nedyalko Boydev <nedyalkoboydev@yahoo.com>
2022-03-13 14:22:48 +0200

    fixed navbar sizing

--- src/components/Header/NavBar.js
+++ src/components/Header/NavBar.js
@@ -2,7 +2,8 @@ import React, { useContext } from 'react'
 import {
     Box, Flex, Icon, useDisclosure, IconButton, Image, Stack,
     Collapse, Badge,
-     useBreakpointValue,
+    useBreakpointValue,
+    Spacer,
 } from '@chakra-ui/react'
 import {
     HamburgerIcon, CloseIcon
@@ -12,7 +13,7 @@ import { MdShoppingBasket } from "react-icons/md"
 import DesktopNav from './DesktopNav'
 import MobileNav from './MobileNav'
 import { ShopContext } from '../../context/ShopContext'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 const NavBar = () => {
     const { openCart, checkout } = useContext(ShopContext);
@@ -25,9 +26,8 @@ const NavBar = () => {
                 align={'center'}>
                 <Flex
                     flex={{ base: 1, md: 'auto' }}
-                    ml={{ base: -2 }}
                     display={{ base: 'flex', md: 'none' }}
-                    w={'20%'}>
+                    >
                     <IconButton
                         onClick={onToggle}
                         icon={
@@ -37,22 +37,22 @@ const NavBar = () => {
                         aria-label={'Toggle Navigation'}
                     />
                 </Flex>
-                <Flex flex={{ base: 1 }} justify={{ base: 'center', md: 'start' }}  w={'20%'}>
-                    <Link to="/"><Image src={require('./JUMPER_logo.png')} w={120} pl={6} align={useBreakpointValue({ base: 'center', md: 'left' })} /></Link>
-                    
+                
+                <Flex  justify={{ base: 'center', md: 'start' }} >
+                    <Link to="/"><Image src={require('./JUMPER_logo.png')} w={120} align={useBreakpointValue({ base: 'center', md: 'left' })} /></Link>
+
                 </Flex>
-                <Flex display={{ base: 'none', md: 'flex' }} ml={10} w={'60%'}>
-                        <DesktopNav />
-                    </Flex>
-  
-                <Stack flex={{ base: 1, md: 0 }}  w={'60%'}
+                <Spacer display={{ base: 'none', md: 'flex' }}/>
+                <Flex display={{ base: 'none', md: 'flex' }} >
+                    <DesktopNav />
+                </Flex>
+                <Spacer display={{ base: 'none', md: 'flex' }}/>
+                <Flex flex={{ base: 1, md: 0 }}
                     justify={'flex-end'}
                     direction={'row'}>
                     <Icon color="#ED8936" cursor="pointer" as={MdShoppingBasket} w={30} h={30} onClick={() => openCart()} />
                     <Badge backgroundColor="#FFFFFF" borderRadius={'100%'} h={4}>{checkout?.lineItems?.length}</Badge>
-                </Stack>
-                
-
+                </Flex>
             </Flex>
             <Collapse in={isOpen} animateOpacity>
                 <MobileNav />
